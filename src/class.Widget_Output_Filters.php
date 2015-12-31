6628613defd392c80985bc9b402925cc57b8afd3
Philip Newcomer <contact@philipnewcomer.net>
2015-12-31 16:43:13 -0500

    Minor refactoring for code style and readablity

--- src/class.Widget_Output_Filters.php
+++ src/class.Widget_Output_Filters.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Class Widget_Output_Filters
  *
@@ -8,50 +7,51 @@
 class Widget_Output_Filters {
 
 	/**
-	 * Widget_Output_Filters constructor.
+	 * Initializes the functionality by registering actions and filters.
 	 */
 	public function __construct() {
-		add_filter( 'dynamic_sidebar_params', array( $this, 'widget_output_filters_dynamic_sidebar_params' ), 9 ); // Priority of 9 to run before the Widget Logic plugin
+
+		// Priority of 9 to run before the Widget Logic plugin.
+		add_filter( 'dynamic_sidebar_params', array( $this, 'filter_dynamic_sidebar_params' ), 9 );
 	}
 
 	/**
 	 * Replaces the widget's display callback with the Dynamic Sidebar Params display callback, storing the original callback for use later.
 	 *
-	 * The $sidebar_params array is not modified; it is only used to get the current widget ID.
+	 * The $sidebar_params variable is not modified; it is only used to get the current widget's ID.
 	 *
-	 * @param  array $sidebar_params The sidebar parameters
-	 * @return array                 The sidebar parameters
+	 * @param array $sidebar_params The sidebar parameters.
+	 *
+	 * @return array The sidebar parameters
 	 */
-	function widget_output_filters_dynamic_sidebar_params( $sidebar_params ) {
+	public function filter_dynamic_sidebar_params( $sidebar_params ) {
 
 		if ( is_admin() ) {
 			return $sidebar_params;
 		}
 
 		global $wp_registered_widgets;
-		$widget_id = $sidebar_params[0]['widget_id'];
+		$current_widget_id = $sidebar_params[0]['widget_id'];
 
-		$wp_registered_widgets[ $widget_id ]['original_callback'] = $wp_registered_widgets[ $widget_id ]['callback'];
-		$wp_registered_widgets[ $widget_id ]['callback'] = array( $this, 'widget_output_filters_display_widget' );
+		$wp_registered_widgets[ $current_widget_id ]['original_callback'] = $wp_registered_widgets[ $current_widget_id ]['callback'];
+		$wp_registered_widgets[ $current_widget_id ]['callback'] = array( $this, 'display_widget' );
 
 		return $sidebar_params;
-
 	}
 
-
 	/**
-	 * Callback function to display the widget's original callback function output, with filtering.
+	 * Execute the widget's original callback function, filtering its output.
 	 */
-	function widget_output_filters_display_widget() {
+	public function display_widget() {
 
 		global $wp_registered_widgets;
 		$original_callback_params = func_get_args();
-		$widget_id = $original_callback_params[0]['widget_id'];
 
+		$widget_id         = $original_callback_params[0]['widget_id'];
 		$original_callback = $wp_registered_widgets[ $widget_id ]['original_callback'];
-		$wp_registered_widgets[ $widget_id ]['callback'] = $original_callback;
 
-		$widget_id_base = $wp_registered_widgets[ $widget_id ]['callback'][0]->id_base;
+		$wp_registered_widgets[ $widget_id ]['callback'] = $original_callback;
+		$widget_id_base = $original_callback[0]->id_base;
 
 		if ( is_callable( $original_callback ) ) {
 
@@ -60,8 +60,6 @@ class Widget_Output_Filters {
 			$widget_output = ob_get_clean();
 
 			echo apply_filters( 'widget_output', $widget_output, $widget_id_base, $widget_id );
-
 		}
-
 	}
 }
